a8243b18343a4d2ed3a36166092b85ad94248936
Tanu Rawat <97741614+tanurawat@users.noreply.github.com>
2024-03-29 15:05:35 +0530

    commit-3

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste React
+
+# The Food Grid
+
+Header
+-logo
+-Nav items
+Body
+-Search
+
+- Restaurant Container
+  --Restaurant card
+  -img
+  -name, star rating, cuisine, delivery time
+  Footer
+- Copyright
+- Links
+- Address
+- Contact
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,24 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    resData?.info;
+
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+        alt="res-logo"
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
